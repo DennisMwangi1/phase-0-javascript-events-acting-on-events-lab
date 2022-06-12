a265a458721a5a48aebf8c8f1f3d102027574170
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-12 21:45:27 +0300

    commit tree 59eb6b9d3b006e1ccc3914cad288b4bfa1065e24

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ dodger.style.right = "10px"
     }
   });
   function moveDodgerRight() {
-    const rightNumbers = dodger.style.right.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
   
-    if (right > 0) {
-      dodger.style.right = `${right - 1}px`;
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
     }
   }
